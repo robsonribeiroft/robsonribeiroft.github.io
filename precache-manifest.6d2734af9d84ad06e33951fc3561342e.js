c4e1c53ace37d53e4e246a4ab1d12fa8cb05fe43
root <root@DKT-000.localdomain>
2019-05-13 17:16:09 -0300

    Updates

---
+++ precache-manifest.6d2734af9d84ad06e33951fc3561342e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0770736297288fc42443552d18e4133e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0461093476738ccadb75",
+    "url": "/static/css/main.f9c24113.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "0461093476738ccadb75",
+    "url": "/static/js/main.51c10c1b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
